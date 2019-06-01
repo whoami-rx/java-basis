abde06c17adc8504b0e540e489a8c5b4e1a1808e
RAR <mem.leaks12@gmail.com>
2019-06-01 18:40:24 -0300

    Java primitives

---
+++ src/main/java/part1/primitives/Primitives.java
@@ -0,0 +1,35 @@
+package part1.primitives;
+
+/**
+ * Primitive: is a data type
+ *
+ * Java have 8 primitives:
+ *
+ * Signed Integers: byte, short, int, long
+ * Float Point: float, double
+ * Character: char
+ * Boolean: boolean
+ */
+public class Primitives {
+    public static void signedIntegers() {
+        byte a = 0;
+        short b = 1;
+        int c = 1;
+        long d = 1;
+    }
+
+    public static void floatPoint() {
+        float a = 1.0f;    // should have postfix 'f'
+        double b = 1.0;
+    }
+
+    public static void charType() {
+        char a = '\t';
+        char b = '\n';
+    }
+
+    public static void boolType() {
+        boolean a = true;
+        boolean b = false;
+    }
+}
