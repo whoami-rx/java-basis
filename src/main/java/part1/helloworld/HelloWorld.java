4cb1ad7886e737ddf78110ab34c3675ce4585f6f
RAR <mem.leaks12@gmail.com>
2019-06-01 18:40:03 -0300

    Hello World

---
+++ src/main/java/part1/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package part1.helloworld;
+
+/**
+ * 1. All methos are placed inside a class
+ * 2. Method 'main' is called first for a program
+ * 3. Package is a group of related classes
+ * 4. Line that starts with // is a comment.
+ * 4. println print message on standard output.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World"
+        System.out.println("Hello, World");
+    }
+}
