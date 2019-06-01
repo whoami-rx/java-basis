910c144606536b7bb3506b9efd52e1c575fbc159
RAR <mem.leaks12@gmail.com>
2019-06-01 18:51:40 -0300

    Unary operators

---
+++ src/main/java/part1/operators/Unary.java
@@ -0,0 +1,13 @@
+package part1.operators;
+
+public class Unary {
+    public static boolean negate(boolean a) {
+        return !a;
+    }
+    public static int increment(int a) {
+        return ++a;
+    }
+    public static int decrement(int a) {
+        return --a;
+    }
+}
